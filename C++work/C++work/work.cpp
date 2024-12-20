4d082f9325bb9c6c198b57016421012f04cb5d94
snake6b6 <2892674765@qq.com>
2024-12-20 22:33:46 +0800

    12.20

---
+++ C++work/C++work/work.cpp
@@ -0,0 +1,210 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+////第一次作业
+//#include<iostream>
+//using namespace std;
+//double fac(double n) {
+//	if (n < 0) {
+//		cout << "input error!";
+//		return -1;
+//	}
+//	else if (n <= 1&&n>=0) {
+//		return 1;
+//	}
+//	else {
+//		return fac(n - 1) * n;
+//	}
+//}
+//int main() {
+//	double a, b, c;
+//	cout << "请分别输入a，b，c的值，并用空格隔开"<<endl;
+//	cin >> a >> b >> c;
+//	cout << "a!+b!+c!=" << fac(a) + fac(b) + fac(c);
+//	return 0;
+// }
+
+////第二次作业
+//#include<iostream>
+//using namespace std;
+// 
+//double funcation(double x) {
+//	if (x < 1) {
+//		return x;
+//	}
+//	else if (x >= 1 && x < 10) {
+//		return 2*x - 1;
+//	}
+//	else
+//	{
+//		return 3 * x - 1;
+//	}
+//}
+//int main() {
+//	double a;
+//	cout << "请输入一个数："<<endl;
+//	cin >> a;
+//	cout << "f("<<a<<")=" << funcation(a);;
+//	return 0;
+//}
+
+////第三次作业
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	double sum = 0;
+//	double fac = 1;
+//	for (int i = 1; i <=20; i++)
+//	{
+//		fac = fac * i;
+//		sum = sum + fac;
+//	}
+//	cout << "1!+2!+3!+...+20!=" << sum;
+//	return 0;
+//}
+//
+
+////第四次作业
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int arr[3][3] = { 0 };
+//	int i = 0, j = 0,sum=0;
+//	cout << "请输入矩阵元素：" << endl;
+//	for (i=0; i < 3; i++) {
+//		for (j=0; j < 3;j ++)
+//		{
+//			cin >> arr[i][j];
+//		}
+//	}
+//	for (i=0; i < 3; i++) {
+//		for (j=0; j < 3; j++)
+//		{
+//			if (i == j||i+j==2) {
+//				sum = sum + arr[i][j];
+//			}
+//		}
+//	}
+//	cout << "对角线元素之和为：" << sum;
+//	return 0;
+// }
+
+////第6次
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int arr[10][10] = { 0 };
+//	int i, j;
+//	for (i = 0; i < 10; i++) {
+//		for ( j = 0; j < i+1; j++)
+//		{
+//			if (i == 0 || i == j||j==0) {
+//				arr[i][j] = 1;
+//			}
+//			else if (i > 1) {
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < i + 1; j++)
+//		{
+//			cout << arr[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+//
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Teacher
+//{
+//public:
+//	Teacher(string name1, int age1, string manl1, string adress1, string phone_num1, string title1);
+//	void display();
+//protected:
+//	string name;
+//	int age;
+//	string manl;
+//	string adress;
+//	string phone_num;
+//	string title;
+//};
+//
+//Teacher::Teacher(string name1, int age1, string manl1, string adress1, string phone_num1, string title1) {
+//	name = name1;
+//	age = age1;
+//	manl = manl1;
+//	adress = adress1;
+//	phone_num = phone_num1;
+//	title = title1;
+//}
+//
+//void Teacher::display() {
+//	cout << name << endl;
+//	cout << age << endl;
+//	cout << manl << endl;
+//	cout << adress << endl;
+//	cout << phone_num << endl;
+//	cout << title<< endl;
+//}
+//
+//
+//class Cadre
+//{
+//public:
+//	Cadre(string name1, int age1, string manl1, string adress1, string phone_num1, string post);
+//	void display();
+//protected:
+//	string name;
+//	int age;
+//	string manl;
+//	string adress;
+//	string phone_num;
+//	string post;
+//};
+//
+//Cadre::Cadre(string name1, int age1, string manl1, string adress1, string phone_num1, string post1)
+//{
+//	name = name1;
+//	age = age1;
+//	manl = manl1;
+//	adress = adress1;
+//	phone_num = phone_num1;
+//	post = post1;
+//}
+//
+//void Cadre::display() {
+//	cout << name<<endl;
+//	cout << age << endl;
+//	cout << manl << endl;
+//	cout << adress << endl;
+//	cout << phone_num << endl;
+//	cout<< post << endl;
+//}
+//
+//
+//class Teacher_Cadre :public Teacher, public Cadre
+//{
+//public:
+//	Teacher_Cadre (string name1, int age1, string manl1, string adress1, string phone_num1, string title1,string post1,double wages1) : 
+//		Teacher(name1,age1,manl1,adress1,phone_num1,title1), Cadre(name1, age1, manl1, adress1, phone_num1,post1) {wages=wages1;};
+//	void show();
+//protected:
+//	double wages;
+//};
+//
+//void Teacher_Cadre::show(){
+//	Teacher::display();
+//	cout << post << endl;
+//	cout << wages << endl;
+//}
+//
+//int main() {
+//	Teacher_Cadre hunman("张三", 30, "男", "北京市某街道", "123456789", "讲师", "教研室主任", 8000.0);
+//	hunman.show();
+//	return 0;
+//}
--- lq2.cpp
+++ lq2.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
+#include<string>
 #include<algorithm>
 using namespace std;
-
-int main(){
-	int count=0;
-	int num=0;
-	string str="";
-	for(int i=1;i<=5;i++){
-		cin>>num;
-		str=to_string(num);
-		if(str[0]==str[2]&&str[3]-str[1]==1){
-			count++;
+bool check(string num, int n) {
+	int i = 0;
+	int temp = 0;
+	for (i=0; i < n; i++) {
+		temp = (int)num[i] - '0';
+		if (i % 2 == 0 && temp % 2 != 1) {
+			return false;
+		}
+		else if (i % 2 == 1 && temp % 2 != 0) {
+			return false;
 		}
 	}
-	cout<<count;
+	return true;
+}
+int main() {
+	long int N = 0, count = 0;
+	string str_num = "";
+	long int i = 0;
+	cin >> N;
+	for (i = 1; i <= N; i++)
+	{
+		str_num = to_string(i);
+		reverse(str_num.begin(), str_num.end());
+		if (check(str_num, str_num.size()))
+			count++;
+	}
+	cout << count;
 	return 0;
 }
